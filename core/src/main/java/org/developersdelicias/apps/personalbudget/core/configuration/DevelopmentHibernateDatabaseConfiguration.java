fa83974733dfc8086a126564c22787f187818e74
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-06-16 01:33:49 -0500

    Add changes to display page and connect to dev environment

---
+++ core/src/main/java/org/developersdelicias/apps/personalbudget/core/configuration/DevelopmentHibernateDatabaseConfiguration.java
@@ -0,0 +1,41 @@
+package org.developersdelicias.apps.personalbudget.core.configuration;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({ "org.developersdelicias.apps.personalbudget.core.model" })
+@Profile("dev")
+public class DevelopmentHibernateDatabaseConfiguration extends AbstractHibernateDatabaseConfiguration {
+    private final static Logger _logger = LoggerFactory.getLogger(DevelopmentHibernateDatabaseConfiguration.class);
+
+    @Override
+    protected DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/personal_budget");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+    @Override
+    protected Properties hibernateProperties() {
+        _logger.info("Creating Properties");
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        properties.put("hibernate.hbm2ddl.auto", "update");
+        _logger.info("Returning Properties");
+        return properties;
+    }
+}
