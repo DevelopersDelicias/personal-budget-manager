1cbddd45850ec790d757a73888f2060b9cb58816
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-06-16 14:36:29 -0500

    Make package-private validate method of UsernameValidator

--- core/src/main/java/org/developersdelicias/apps/personalbudget/core/model/user/UsernameValidator.java
+++ core/src/main/java/org/developersdelicias/apps/personalbudget/core/model/user/UsernameValidator.java
@@ -3,7 +3,7 @@ package org.developersdelicias.apps.personalbudget.core.model.user;
 import static java.util.Objects.isNull;
 
 class UsernameValidator {
-    public boolean validate(String username) {
+    boolean validate(String username) {
         return !isNull(username) && username.matches("(?!.*([_])\\1)[a-z][a-z0-9_]{5,14}");
     }
 }
