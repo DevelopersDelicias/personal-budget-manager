13115cd4bca08d407a28d82c5ef71993fcd75978
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-06-15 20:19:57 -0500

    Remove length variable from the implementation

--- core/src/main/java/org/developersdelicias/apps/personalbudget/core/model/user/UsernameValidator.java
+++ core/src/main/java/org/developersdelicias/apps/personalbudget/core/model/user/UsernameValidator.java
@@ -4,9 +4,9 @@ import java.util.regex.Pattern;
 
 public class UsernameValidator {
     public boolean validate(String username) {
-        int length = username.length();
-        boolean startsWithALowercase = Pattern.compile("^[a-z]+[a-z0-9_]*").matcher(username).matches();
+        String pattern = "^[a-z]+[a-z0-9_]{5,14}";
+        boolean startsWithALowercase = Pattern.compile(pattern).matcher(username).matches();
         boolean hasConsecutiveUnderScores = Pattern.compile(".*[_]{2,}.*").matcher(username).matches();
-        return startsWithALowercase && length >= 6 && length <= 15 && !hasConsecutiveUnderScores;
+        return startsWithALowercase  && !hasConsecutiveUnderScores;
     }
 }
