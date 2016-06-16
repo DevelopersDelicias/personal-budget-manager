da8839c7d17b7467d4138b0356f8f68512546b7b
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-06-15 20:37:24 -0500

    Refactor validate method to use a single pattern

--- core/src/main/java/org/developersdelicias/apps/personalbudget/core/model/user/UsernameValidator.java
+++ core/src/main/java/org/developersdelicias/apps/personalbudget/core/model/user/UsernameValidator.java
@@ -1,12 +1,7 @@
 package org.developersdelicias.apps.personalbudget.core.model.user;
 
-import java.util.regex.Pattern;
-
 public class UsernameValidator {
     public boolean validate(String username) {
-        String pattern = "^[a-z]+[a-z0-9_]{5,14}";
-        boolean startsWithALowercase = Pattern.compile(pattern).matcher(username).matches();
-        boolean hasConsecutiveUnderScores = Pattern.compile(".*[_]{2,}.*").matcher(username).matches();
-        return startsWithALowercase  && !hasConsecutiveUnderScores;
+        return username.matches("(?!.*([_])\\1)[a-z][a-z0-9_]{5,14}");
     }
 }
