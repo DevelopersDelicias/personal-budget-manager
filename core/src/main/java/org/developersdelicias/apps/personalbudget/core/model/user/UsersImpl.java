e9ecb5d7dd3600378aba63b9f110d03c677043d5
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-06-16 21:41:31 -0500

    Refactor method for geeting the session from the current sessionFactory

--- core/src/main/java/org/developersdelicias/apps/personalbudget/core/model/user/UsersImpl.java
+++ core/src/main/java/org/developersdelicias/apps/personalbudget/core/model/user/UsersImpl.java
@@ -18,26 +18,24 @@ public class UsersImpl implements Users {
     @Override
     @SuppressWarnings("unchecked")
     public List<User> allUsers() {
-        Session session = sessionFactory.getCurrentSession();
-        return session.createQuery("from User").list();
+        return session().createQuery("from User").list();
     }
 
     @Override
     public void add(User newUser) {
-        Session session = sessionFactory.getCurrentSession();
-        session.persist(newUser);
+        session().persist(newUser);
     }
 
     @Override
     public void update(User user) {
-        Session session = sessionFactory.getCurrentSession();
+        Session session = session();
         session.update(user);
         session.flush();
     }
 
     @Override
     public void remove(long id) {
-        Session session = sessionFactory.getCurrentSession();
+        Session session = session();
         session.createQuery("DELETE FROM User u WHERE u.id = :id")
                 .setParameter("id", id)
                 .executeUpdate();
@@ -46,8 +44,7 @@ public class UsersImpl implements Users {
 
     @Override
     public User findById(long id) {
-        Session session = sessionFactory.getCurrentSession();
-        User user = session.get(User.class, id);
+        User user = session().get(User.class, id);
         validate(user);
         return user;
     }
@@ -55,14 +52,17 @@ public class UsersImpl implements Users {
     @Override
     @SuppressWarnings("unchecked")
     public User findByUsername(String username) {
-        Session session = sessionFactory.getCurrentSession();
-        List<User> list = session.createQuery("FROM User u WHERE u.username=:username")
+        List<User> list = session().createQuery("FROM User u WHERE u.username=:username")
                 .setParameter("username", username)
                 .list();
         validateIsNotEmpty(list);
         return list.get(0);
     }
-    
+
+    private Session session() {
+        return sessionFactory.getCurrentSession();
+    }
+
     private void validateIsNotEmpty(List<User> list) {
         if (list.isEmpty()) {
             throw new UserNotFoundException();
