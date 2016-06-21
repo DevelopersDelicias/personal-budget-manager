26a05d161faa5bae46d60ba085c065863d4dc9ae
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-06-20 19:56:06 -0500

    Refactor EmailValidator.validate(email) method

--- core/src/main/java/org/developersdelicias/apps/personalbudget/core/model/user/EmailValidator.java
+++ core/src/main/java/org/developersdelicias/apps/personalbudget/core/model/user/EmailValidator.java
@@ -1,12 +1,8 @@
 package org.developersdelicias.apps.personalbudget.core.model.user;
 
-import java.util.regex.Pattern;
-
-import static java.util.regex.Pattern.compile;
-
 public class EmailValidator {
     public boolean validate(String email) {
-        return compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$").matcher(email).matches();
+        return email.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
     }
 }
