111e85ce2087238d60871bef2bce9eedb156c12c
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-06-21 21:38:02 -0500

    Add test to check the input email is present

--- web/src/test/java/org/developersdelicias/apps/personalbudget/web/features/login/LoginFeatureShould.java
+++ web/src/test/java/org/developersdelicias/apps/personalbudget/web/features/login/LoginFeatureShould.java
@@ -1,10 +1,12 @@
 package org.developersdelicias.apps.personalbudget.web.features.login;
 
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 public class LoginFeatureShould extends AbstractFeature {
@@ -26,5 +28,11 @@ public class LoginFeatureShould extends AbstractFeature {
     @Test
     public void have_a_title() {
         assertThat(homePage.getTitleText(), is("Personal Budget Manager"));
+    }
+
+    @Test
+    public void have_an_email_input() throws Exception {
+        HtmlInput emailInput = homePage.getFirstByXPath("//input[@type='email']");
+        assertNotNull(emailInput);
     }
 }
